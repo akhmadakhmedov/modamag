a7ecd3af24fb1458a216b2f3949593152dcab3a3
akhmadakhmedov <ahmedov.thy@gmail.com>
2021-10-10 20:42:26 +0600

    Decouple installed

--- modamag/settings.py
+++ modamag/settings.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = [
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-
+DEFAULT_FILE_STORAGE = 'modamag.media_storages.MediaStorage'
 # Media files configuration
 
 MEDIA_URL = '/media/'
