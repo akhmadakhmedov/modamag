de346f26ac86183ee3a768aac38f7c3c23bc169a
akhmadakhmedov <ahmedov.thy@gmail.com>
2021-10-10 17:06:07 +0600

    pyscopg added to requirements.txt

--- modamag/settings.py
+++ modamag/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) #True
 
-ALLOWED_HOSTS = ['skido-env.eba-3pxyppjj.eu-central-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['skido-env.eba-3pxyppjj.eu-central-1.elasticbeanstalk.com', '*']
 
 
 # Application definition
