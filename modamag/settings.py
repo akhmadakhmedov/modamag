9b783e81aa89cb3553becc6b761a8eec8757234c
akhmadakhmedov <ahmedov.thy@gmail.com>
2021-10-11 19:35:45 +0600

    deploying to s3

--- modamag/settings.py
+++ modamag/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) #True
 
-ALLOWED_HOSTS = ['skido-env.eba-gzwphc7m.eu-central-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['skido-env.eba-gzwphc7m.eu-central-1.elasticbeanstalk.com', '*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'admin_honeypot',
+    'storages',
 
 ]
 
@@ -144,16 +145,31 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+#STATIC_ROOT = BASE_DIR /'static'
+#STATICFILES_DIRS = [
+#    'modamag/static',]
 
-STATIC_ROOT = BASE_DIR /'static'
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
     'modamag/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-#DEFAULT_FILE_STORAGE = 'modamag.media_storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'modamag.media_storages.MediaStorage'
 # Media files configuration
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 # Default primary key field type
