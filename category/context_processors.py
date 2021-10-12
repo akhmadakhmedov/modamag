d0b469adb3163c2b599730ea33149e001410497c
akhmadakhmedov <ahmedov.thy@gmail.com>
2021-10-12 13:47:30 +0600

    Categories in navbar increased

--- category/context_processors.py
+++ category/context_processors.py
@@ -2,6 +2,6 @@ from .models import Category
 
 def menu_links(request):
     links = Category.objects.all()
-    linki = Category.objects.all().order_by('-id')[:4]
+    linki = Category.objects.all().order_by('-id')[:8]
     return dict(links = links, linki=linki)
 
